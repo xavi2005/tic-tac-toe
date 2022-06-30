456dbaeccbb0ccaf474a6fe54ee1a949189cbac6
xavi2005 <94165445+xavi2005@users.noreply.github.com>
2022-07-01 00:40:32 +0200

    Create constantes.py

---
+++ constantes.py
@@ -0,0 +1,26 @@
+#CANVAS
+
+ROW = 3
+COL = 3
+WIDTH = 600
+HEIGHT = 600
+WIDTH_P = WIDTH / 3
+HEIGHT_P = HEIGHT / 3
+ANCH = 4
+
+SIZE = (WIDTH,HEIGHT)
+RECORT = 35 #recorta las cruzes para centrarlas
+
+#COLORS
+
+BLACK = (0,0,0)
+CYAN = (0,255,255)
+SCREEN = (25,178,155)
+COLOR_LINE = (25,146,136)
+WHITE = (230,250,240)
+
+#BOARD
+
+BOARD = [[0,0,0]
+        ,[0,0,0],
+         [0,0,0]]
